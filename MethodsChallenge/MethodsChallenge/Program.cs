99a88815f076ba672eba60487f15732ddfe83708
REDS031 <lucas.santos@redspark.io>
2018-08-17 13:38:48 -0300

    Already created projects locally, so now i'm sending them to remote branch

---
+++ MethodsChallenge/MethodsChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MethodsChallenge
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string name1 = "Lucas Medina";
+            string name2 = "Leonardo Pereira";
+            string name3 = "Everton Robozão";
+
+            GreetFriend(name1);
+            GreetFriend(name2);
+            GreetFriend(name3);
+            Console.Read();
+        }
+
+        public static void GreetFriend(string personName)
+        {
+            Console.WriteLine("Hi " + personName + ", my friend!");
+        }
+    }
+}
---
+++ MethodsReturnValue/MethodsReturnValue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsReturnValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Multiply(25, 25));
+            Console.WriteLine(TellName("Lucas Medina"));
+            Console.WriteLine(Divide(13, 10));
+            Console.Read();
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static string TellName(string name)
+        {
+            return "Your name is " + name;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
